d9212d8fe27b1bf1a1a8a4297fec2e42f0a66497
dominique.johnson14@yahoo.com <dominique.johnson14@yahoo.com>
2020-08-30 14:41:56 -0500

    Added the DOTweeen asset

---
+++ Assets/Dom's Stuff/Scripts/DOTweenController.cs
@@ -0,0 +1,49 @@
+﻿using DG.Tweening;
+using System.Collections;
+using UnityEngine;
+
+public class DOTweenController : MonoBehaviour
+{
+    [SerializeField] private Vector3 target = Vector3.zero;
+    [SerializeField] private float MoveDuration = 1f;
+    [SerializeField] private Ease MoveEase = Ease.Linear;
+    [SerializeField] private DOTweenType _DoTweenType = DOTweenType.MovementOneWay;
+
+    public GameObject Moveable;
+
+    private enum DOTweenType
+    {
+        MovementOneWay,
+        MovementTwoWay,
+    }
+
+    private void Start()
+    {
+        if (_DoTweenType == DOTweenType.MovementOneWay)
+        {
+            if (target == Vector3.zero)
+            {
+                target = transform.position;
+                Moveable.transform.DOMove(target, MoveDuration).SetEase(MoveEase);
+            }
+        }
+
+        else if (_DoTweenType == DOTweenType.MovementTwoWay)
+        {
+            if (target == Vector3.zero)
+            {
+                target = transform.position;
+                StartCoroutine(MoveBothWays());
+            }
+        }
+    }
+
+    private IEnumerator MoveBothWays()
+    {
+        Vector3 OrginalLocation = transform.position;
+        transform.DOMove(target, MoveDuration).SetEase(MoveEase);
+        yield return new WaitForSeconds(MoveDuration);
+        transform.DOMove(OrginalLocation, MoveDuration).SetEase(MoveEase);
+    }
+
+}
