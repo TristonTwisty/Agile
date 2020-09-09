22e7363872ce6feabbe81c70fbea12654fd7c578
dominique.johnson14@yahoo.com <dominique.johnson14@yahoo.com>
2020-09-09 02:12:41 -0500

    Created an AI script that patrols and area until the player hits a trigger. The AI then chases the player

---
+++ Assets/Dom's Stuff/Scripts/EnemyGroup.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGroup : MonoBehaviour
+{
+    public Transform Player;
+    public GameObject[] Enemies;
+
+    private void Awake()
+    {
+        foreach (GameObject enemy in Enemies)
+        {
+            enemy.GetComponent<EnemyMovement>().Target = Player;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            foreach(GameObject enemy in Enemies)
+            {
+                enemy.GetComponent<EnemyMovement>().AttackPlayer = true;
+            }
+        }
+    }
+}
---
+++ Assets/Dom's Stuff/Scripts/EnemyMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+
+public class EnemyMovement : MonoBehaviour
+{
+    public Transform[] PatrolPoints;
+    private int DestinationPoint;
+    private NavMeshAgent Agent;
+
+    [HideInInspector] public bool AttackPlayer = false;
+    [HideInInspector] public Transform Target;
+
+    private void Start()
+    {
+        Agent = GetComponent<NavMeshAgent>();
+        Agent.autoBraking = false;
+
+        DoPatrol();
+    }
+
+    private void Update()
+    {
+        if(!AttackPlayer)
+        {
+            if(!Agent.pathPending && Agent.remainingDistance < .5f)
+            {
+                DoPatrol();
+            }
+        }
+        else
+        {
+            Agent.destination = Target.position;
+        }
+    }
+
+    private void DoPatrol()
+    {
+        if (PatrolPoints.Length == 0)
+            return;
+
+        Agent.destination = PatrolPoints[DestinationPoint].position;
+        DestinationPoint = (DestinationPoint + 1) % PatrolPoints.Length;
+    }
+}
