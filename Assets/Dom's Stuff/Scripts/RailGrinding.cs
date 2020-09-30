4cc0248937ece9bb3c3aa06e4e2fd2dee15aa6f8
dominique.johnson14@yahoo.com <dominique.johnson14@yahoo.com>
2020-09-30 10:29:18 -0500

    Fixed grinding script not grinding

--- Assets/Dom's Stuff/Scripts/RailGrinding.cs
+++ Assets/Dom's Stuff/Scripts/RailGrinding.cs
@@ -8,33 +8,35 @@ public class RailGrinding : MonoBehaviour
     [SerializeField] private GameObject Hoverboard;
     [SerializeField] private float Speed = 5f;
     [Tooltip("Rail particle system to let player know they can grind")][SerializeField] private ParticleSystem RailParticle;
+    private bool CanGrind = false;
 
     [Header("Tweener")]
     [Tooltip("How the board follows the rail path")][SerializeField] private PathType Rail = PathType.CatmullRom;
     [Tooltip("Put in the rail waypoints to create rail path")][SerializeField] private Vector3[] PathValue = new Vector3[3];
+
+    private void Update()
+    {
+        if (CanGrind && Input.GetKeyDown("e"))
+        {
+            Hoverboard.GetComponent<HoverboardInput>().enabled = false;
+            Hoverboard.transform.DOPath(PathValue, Speed, Rail);
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Hoverboard"))
         {
             RailParticle.Play();
-        }
-
-        if (other.gameObject.CompareTag("Hoverboard"))
-        {
-            if (Input.GetKeyDown("e"))
-            {
-                Hoverboard.GetComponent<HoverboardInput>().enabled = false;
-                Hoverboard.transform.DOPath(PathValue, Speed, Rail);
-            }
+            CanGrind = true;
         }
     }
-
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.CompareTag("Hoverboard"))
         {
             RailParticle.Stop();
+            CanGrind = false;
         }
     }
 }
