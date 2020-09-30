bb32a665970d0ba5e4e22da6fa5e99e55c5a0ae7
Twisty10000 <tschro13@stumail.jccc.edu>
2020-09-30 12:53:27 -0500

    Added charachter controller. and made the board gettable onable and gettable offable!

---
+++ Assets/GetOnBoard.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetOnBoard : MonoBehaviour
+{
+    public GameObject BoardCamera;
+    public GameObject Board;
+    private Component BoardController;
+    public GameObject Player;
+    public GameObject Boardrider;
+
+
+    private void Start()
+    {
+        BoardController = gameObject.GetComponent<HoverboardInput>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        BoardCamera.gameObject.SetActive(true);
+        GetComponent<HoverboardInput>().enabled = true;
+        Boardrider.gameObject.SetActive(true);
+        Player.gameObject.SetActive(false);
+        Player.transform.parent = Board.transform;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            BoardCamera.gameObject.SetActive(false);
+            Board.gameObject.SetActive(false);
+            GetComponent<HoverboardInput>().enabled = false;
+            Boardrider.gameObject.SetActive(false);
+            Player.gameObject.SetActive(true);
+            Player.transform.parent = null;
+        }
+    }
+}
