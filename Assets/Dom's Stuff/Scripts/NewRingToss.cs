8d0f4b4c54cfd5549c5eb0d7a7b307b889526ba0
dominique.johnson14@yahoo.com <dominique.johnson14@yahoo.com>
2020-10-03 08:45:55 -0500

    Reworked the ring throw script and rigiod body to work with character controller

--- Assets/Dom's Stuff/Scripts/NewRingToss.cs
+++ Assets/Dom's Stuff/Scripts/NewRingToss.cs
@@ -8,21 +8,28 @@ using UnityEngine;
 
 public class NewRingToss : MonoBehaviour
 {
-    [SerializeField] private Transform Player;
-    [SerializeField] private float BounceAmount = 500;
-    [SerializeField] private float ReturnSpeed;
+    [Header("View")]
+    [SerializeField] [Tooltip("The object the ring follows")] private Transform RingHolster;
+    [SerializeField] [Tooltip("The trail renderer of the disc")] private TrailRenderer TR;
+
+    [Header("Mechanics")]
+    [SerializeField] [Tooltip("How fast the ring travels after being thrown")] private float ThrowSpeed = 500;
+    [SerializeField] [Tooltip("How how seconds it takes for ring to return to player")] private float ReturnSpeed;
     [SerializeField] [Tooltip("How far the disc can travel before returning to player")] private float MaxDistance;
 
     private Rigidbody RB;
     private BoxCollider BC;
     private Vector3 LastVelocity;
-    private bool Thrown = false;
+    private bool Thrown = false; // Has the ring been thrown?
     private bool DoReturn = false;
 
     private void Start()
     {
+        // Get ring's collider and rigidbody
+        // Set ring's collider to trigger
         RB = GetComponent<Rigidbody>();
         BC =GetComponent<BoxCollider>();
+        TR.enabled = false;
         BC.isTrigger = true;
     }
 
@@ -30,12 +37,21 @@ public class NewRingToss : MonoBehaviour
     {
         if (!Thrown && Input.GetMouseButtonDown(0))
         {
+            // If the ring was NOT thrown and player hits left mouse, throw ring
             ThrowDisc();
+        }
+        if (!Thrown)
+        {
+            // If ring was not thrown, make the ring follow the ring holster
+            transform.position = Vector3.MoveTowards(transform.position, RingHolster.position, 1);
         }
         if (Thrown)
         {
-            if (Input.GetMouseButtonDown(1) || Vector3.Distance(transform.position, Player.position) > MaxDistance)
+            // Check whether the ring was thrown
+            if (Input.GetMouseButtonDown(1) || Vector3.Distance(transform.position, RingHolster.position) > MaxDistance)
             {
+                // If the ring is too far from the player, return the ring to the player
+                // If the player hits the right mouse, return the ring to the player
                 DoReturn = true;
             }
         }
@@ -49,22 +65,35 @@ public class NewRingToss : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (Thrown && other.gameObject.CompareTag("Player"))
         {
+            // Change the ring's collider to collision when it leaves the player's collider
             BC.isTrigger = false;
+
+            //Turn on trail renderer once the ring leaves the player
+            TR.enabled = true;
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (Thrown && other.gameObject.CompareTag("Player"))
         {
+            // Trail renderer turns off once it hits the player
+            TR.enabled = false;
+
             Thrown = false;
             DoReturn = false;
+
+            // Stop all physics movement of the ring
             RB.velocity = Vector3.zero;
             RB.angularVelocity = Vector3.zero;
-            transform.position = Vector3.MoveTowards(transform.position, Player.position, 1);
-            transform.parent = Player;
+
+            // Change ring's rotation to match holster
+            // Could also just freeze rigibody's rotation
+            transform.rotation = Quaternion.Euler(90, 0, 0);
+
+            transform.position = Vector3.MoveTowards(transform.position, RingHolster.position, 1);
         }
     }
 
@@ -74,19 +103,25 @@ public class NewRingToss : MonoBehaviour
         Vector3 Direction = Vector3.Reflect(LastVelocity.normalized, collision.contacts[0].normal);
 
         RB.velocity = Direction * Mathf.Max(Speed, 0);
+
+        // If the ring hits the player, return ring to player
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            DoReturn = true;
+        }
     }
 
     private void ThrowDisc()
     {
         Thrown = true;
-        transform.parent = null;
-        RB.AddForce(Camera.main.transform.forward * BounceAmount, ForceMode.Force);
+        RB.AddForce(Camera.main.transform.forward * ThrowSpeed, ForceMode.Force);
     }
 
     private void ReturnDisc()
     {
+        // Set ring back to trigger so it doesn't knock over player
         BC.isTrigger = true;
-        Vector3 Destination = Player.position - transform.position;
+        Vector3 Destination = RingHolster.position - transform.position;
         RB.velocity = Destination * ReturnSpeed;
     }
 }
