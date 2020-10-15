e21f5dbe07db0dabd69d45f8c1115f991b5283f1
dominique.johnson14@yahoo.com <dominique.johnson14@yahoo.com>
2020-10-15 17:50:38 -0500

    Finished baking lights for my level. Updated the powerup collecting script

--- Assets/Dom's Stuff/Scripts/CollectPowerup.cs
+++ Assets/Dom's Stuff/Scripts/CollectPowerup.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class CollectPowerup : MonoBehaviour
 {
-    [SerializeField] private GameObject Shield;
-    [SerializeField] private bool CollectShield = false;
+    [SerializeField] private GameObject ParticleDisc = null;
+    [SerializeField] private bool CollectDisc;
+
+    [SerializeField] private GameObject ParticleShield = null;
+    [SerializeField] private bool CollectShield;
+
+    [SerializeField] private GameObject DashBelt = null;
+    [SerializeField] private bool CollectBelt;
+
+    //[SerializeField] private GameObject Hoverboard = null;
+    //[SerializeField] private bool CollectBoard;
+
+    [SerializeField] private GameObject MagneticBoots = null;
+    [SerializeField] private bool CollectBoots;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             if (CollectShield)
             {
-                Shield.SetActive(true);
+                ParticleShield.SetActive(true);
+                Destroy(gameObject);
+            }
+            if (CollectBelt)
+            {
+                DashBelt.SetActive(true);
+                Destroy(gameObject);
+            }
+            if (CollectDisc)
+            {
+                ParticleDisc.SetActive(true);
+                Destroy(gameObject);
+            }
+            if (CollectBoots)
+            {
+                MagneticBoots.SetActive(true);
                 Destroy(gameObject);
             }
         }
