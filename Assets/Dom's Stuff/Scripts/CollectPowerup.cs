fd74f7c5fad3471c76bdb14d8dda80a3fa0208e3
dominique.johnson14@yahoo.com <dominique.johnson14@yahoo.com>
2020-10-15 05:10:52 -0500

    Finished the district one level. Created a 1st to 3rd person camera transitioning script. Created a powerup collecting script

---
+++ Assets/Dom's Stuff/Scripts/CollectPowerup.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectPowerup : MonoBehaviour
+{
+    [SerializeField] private GameObject Shield;
+    [SerializeField] private bool CollectShield = false;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (CollectShield)
+            {
+                Shield.SetActive(true);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
