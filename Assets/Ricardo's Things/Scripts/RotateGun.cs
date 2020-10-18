a56228ae18bc89bc34de9c7a4b63a85bc4f6e96d
ricky70281 <nmaharaj@stumail.jccc.edu>
2020-10-18 10:42:19 -0500

    Can now let go and grappling multiple points, swings smoothly, still 2 errors need to iron out, line not lining up, grappling on wrong mask

---
+++ Assets/Ricardo's Things/Scripts/RotateGun.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateGun : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Grappling grapplinggun;
+
+    private Quaternion desiredRotation;
+    private float rotationSpeed = 5f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (!grapplinggun.IsGrappling()) {
+            desiredRotation = transform.parent.rotation;
+        }
+
+        else {
+            desiredRotation = Quaternion.LookRotation(grapplinggun.GetGrapplePoint() - transform.position);
+        }
+
+
+        transform.rotation = Quaternion.Lerp(a: transform.rotation, b: desiredRotation, t: Time.deltaTime * rotationSpeed);
+
+        //transform.LookAt(worldPosition: grapplinggun.GetGrapplePoint());
+    }
+}
