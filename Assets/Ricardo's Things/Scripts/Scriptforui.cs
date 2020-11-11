d3bb672e30136fa9cbd5ca7d6a6d51c27ef3f8cc
ricky70281 <nmaharaj@stumail.jccc.edu>
2020-11-11 11:01:34 -0600

    Worked on U.I. and Edited gladiator level

---
+++ Assets/Ricardo's Things/Scripts/Scriptforui.cs
@@ -0,0 +1,53 @@
+﻿using HutongGames.PlayMaker.Actions;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Policy;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class Scriptforui : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Text displayPlayerHealth;
+    public float playerHealth;
+    public int totalDashAmount;
+    public int currentDashAmount;
+    public Text displayTotalDashAmount;
+    public UnityEngine.UI.Slider playerHealthSlider;
+    public UnityEngine.UI.Image dashRecharge;
+   
+    void Start()
+    {
+        currentDashAmount = 0;
+        totalDashAmount = 2;
+        playerHealth = 100;
+ 
+
+
+
+        playerHealthSlider.value = playerHealth;
+        displayPlayerHealth.text = playerHealth.ToString();
+        displayTotalDashAmount.text = currentDashAmount.ToString();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerHealth = playerHealthSlider.value;
+        displayPlayerHealth.text = playerHealth.ToString();
+        
+        if(currentDashAmount < totalDashAmount)
+        {
+            dashRecharge.fillAmount = (dashRecharge.fillAmount + .001f);
+            
+            if(dashRecharge.fillAmount == 1)
+            {
+                currentDashAmount = currentDashAmount + 1;
+                displayTotalDashAmount.text = currentDashAmount.ToString();
+                dashRecharge.fillAmount = 0;
+            }
+        }
+    }
+}
