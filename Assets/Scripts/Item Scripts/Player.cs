b6bbc823df1017180569ae97d7855adbb68492ef
Twisty10000 <tschro13@stumail.jccc.edu>
2021-02-02 21:05:38 -0600

    Worked on inventory

---
+++ Assets/Scripts/Item Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private WhipItem _whip = new WhipItem();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            {
+            _whip.UseItem(transform.position);
+            }
+    }
+}
---
+++ Assets/Scripts/Item Scripts/WhipItem.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhipItem : ItemBase
+{
+    public override void UseItem(Vector3 posistion)
+    {
+        GameObject.Instantiate(VisualManager.instace.WhipEffect,posistion,Quaternion.identity);
+    }
+
+}
