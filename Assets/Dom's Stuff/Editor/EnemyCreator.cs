b77ec71d95eb447ee7d4a3eeb9c87f1231839ef1
dominique.johnson14@yahoo.com <dominique.johnson14@yahoo.com>
2021-03-12 07:24:55 -0600

    Plugged holes in the tunnels. Connected District 1 to Tunnels

--- Assets/Dom's Stuff/Editor/EnemyCreator.cs
+++ Assets/Dom's Stuff/Editor/EnemyCreator.cs
@@ -9,24 +9,27 @@ namespace Dom.EnemyCreator
         [MenuItem("GameObject/Enemy Creator/Ranged Enemy", false, 0)]
         public static void CreateRangedEnemy()
         {
+            GameObject Shooter = new GameObject("Shooter Enemy", typeof(Rigidbody), typeof(CapsuleCollider), typeof(NavMeshAgent), typeof(EnemyBehavior), typeof(ShootingAI));
             // Focus the newly created character
-            //Selection.activeGameObject = Shooter;
+            Selection.activeGameObject = Shooter;
             SceneView.FrameLastActiveSceneView();
         }
 
         [MenuItem("GameObject/Enemy Creator/Melee Enemy", false, 0)]
         public static void CreateMeleeEnemy()
         {
+            GameObject Melee = new GameObject("Melee Enemy", typeof(Rigidbody), typeof(CapsuleCollider), typeof(NavMeshAgent), typeof(EnemyBehavior), typeof(MeleeAI));
             // Focus on created character
-            //Selection.activeGameObject = Melee;
+            Selection.activeGameObject = Melee;
             SceneView.FrameLastActiveSceneView();
         }
 
         [MenuItem("GameObject/Enemy Creator/Drone Enemy", false, 0)]
         public static void CreateDroneEnemy()
         {
+            GameObject Drone = new GameObject("Drone Enemy", typeof(Rigidbody), typeof(CapsuleCollider), typeof(NavMeshAgent), typeof(EnemyBehavior), typeof(DroneAI));
             // Focus on created character
-            //Selection.activeGameObject = Drone;
+            Selection.activeGameObject = Drone;
             SceneView.FrameLastActiveSceneView();
         }
     }
