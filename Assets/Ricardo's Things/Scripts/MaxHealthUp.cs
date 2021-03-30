05ed653067a2c11ef2ef3aeb567e211698ea8378
ricky70281 <nmaharaj@stumail.jccc.edu>
2021-03-30 18:18:29 -0500

    Lots of UI work, edited rich town alot

---
+++ Assets/Ricardo's Things/Scripts/MaxHealthUp.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaxHealthUp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider Player)
+    {
+        Debug.Log("player entered");
+        PlayerRefs.instance.PlayerHealth += 10;
+        PlayerRefs.instance.currentHealth = PlayerRefs.instance.PlayerHealth;
+        Scriptforui.instance.playerHealthSlider.maxValue = PlayerRefs.instance.currentHealth;
+        Scriptforui.instance.playerHealthSlider.value = Scriptforui.instance.playerHealthSlider.maxValue;
+        Destroy(gameObject);
+
+    }
+}
